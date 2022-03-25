bbe499ea090c227505f59e426d6fe5367773a7a8
ArvikVan <arvikvan@gmail.com>
2022-03-25 10:32:44 +0300

    init

---
+++ src/main/java/arvik/springmicroserv/SpringmicroservApplication.java
@@ -0,0 +1,13 @@
+package arvik.springmicroserv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmicroservApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmicroservApplication.class, args);
+	}
+
+}
---
+++ src/test/java/arvik/springmicroserv/SpringmicroservApplicationTests.java
@@ -0,0 +1,13 @@
+package arvik.springmicroserv;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmicroservApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
